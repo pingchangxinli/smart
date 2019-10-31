73496ca1f3a860a8340b4254ee62514924dba42a
haitao.li <haitao.li@eiltesland.com>
2019-10-31 18:48:36 +0800

    调整租户信息controller

--- smart-business/smart-user/src/main/java/com/lee/tenant/controller/TenantController.java
+++ smart-business/smart-user/src/main/java/com/lee/tenant/controller/TenantController.java
@@ -18,14 +18,12 @@ import com.lee.tenant.service.TenantService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * 租户管理
@@ -44,13 +42,6 @@ public class TenantController {
 
     /**
      * 新增租户
-     * http://127.0.0.1:8300/user-api/tenant?access_token=f09fba50-2ad2-432a-95dc-44e61160e1fa
-     * {
-     * "name":"1111",
-     * "company_name":"sdfasdfas",
-     * "domain":"a.b.com"
-     * }
-     *
      * @param tenant 租户
      * @return
      */
@@ -69,8 +60,7 @@ public class TenantController {
     }
 
     /**
-     * http://127.0.0.1:8300/user-api/tenant?access_token=be60825d-f94a-48dd-ba1d-b6dde0ff3329
-     * {"id": "1","name":"name1","status":"0"}
+     * 更新租户信息
      * @param tenant 租户信息
      * @return
      */
@@ -84,23 +74,6 @@ public class TenantController {
         return BaseResponse.builder().data(count).subCode(String.valueOf(httpStatus.value()))
                 .subMsg(httpStatus.getReasonPhrase()).build();
     }
-
-    /**
-     * 查询所有租户
-     * http://127.0.0.1:8300/user-api/tenant/list?access_token=7bbd5508-66e6-42ee-b6e6-dade9dcad82b
-     *
-     * @return 租户集合
-     */
-    @Deprecated
-    @GetMapping("/list")
-    public BaseResponse list(Tenant tenant) {
-        if (log.isDebugEnabled()) {
-            log.debug("[Tenant contorller],tenant:{}",tenant);
-        }
-        List<Tenant> list = tenantService.list(tenant);
-        return BaseResponse.builder().data(list).build();
-    }
-
     /**
      * 分页查询租户信息
      * @param pagination 分页信息
@@ -129,8 +102,6 @@ public class TenantController {
 
     /**
      * 通过ID查询租户
-     * http://127.0.0.1:8300/user-api/tenant/1?access_token=7bbd5508-66e6-42ee-b6e6-dade9dcad82b
-     *
      * @param id 租户ID
      * @return 租户信息
      */
@@ -167,7 +138,6 @@ public class TenantController {
      * http://127.0.0.1:8300/user-api/tenant/domain/id?domain=127.0.0
      * .1&access_token=f855e6e3-ec83-4014-add3-db83f07f9fcd
      * 根据域名获取租户I
-     *
      * @param domain
      * @return
      */

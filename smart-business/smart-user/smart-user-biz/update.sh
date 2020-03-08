d568ca95ea53c88981d893088e2d6f7ade2bedb5
lee <pingchangxinli@126.com>
2020-03-08 10:03:54 +0800

    新增更新文件

---
+++ smart-business/smart-user/smart-user-biz/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mvn clean  package
+
+if [ $? -eq 0 ]
+then
+  echo "mvn 执行成功"
+  cd ./target
+  if [ $? -eq 0 ]
+  then
+    echo "进入target成功，开始上传"
+    scp -P22 ./smart-user-biz-0.0.1-SNAPSHOT.war root@122.112.245.67:/home/smart/smart-user/webapps/ROOT
+    if [ $? -eq 0 ]
+    then
+      echo "上传成功"
+    else
+      echo "上传失败"
+    fi
+  else
+    echo "target目录不存在"
+  fi
+else
+echo "执行失败"
+fi
---
+++ smart-gateway/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mvn clean  package
+
+if [ $? -eq 0 ]
+then
+  echo "mvn 执行成功"
+  cd ./target
+  if [ $? -eq 0 ]
+  then
+    echo "进入target成功，开始上传"
+    scp -P22 ./smart-gateway-0.0.1-SNAPSHOT.jar root@122.112.245.67:/home/smart/smart-gateway
+    if [ $? -eq 0 ]
+    then
+      echo "上传成功"
+    else
+      echo "上传失败"
+    fi
+  else
+    echo "target目录不存在"
+  fi
+else
+echo "执行失败"
+fi
